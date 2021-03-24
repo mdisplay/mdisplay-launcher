0b34ee646e2fa525840ce6ec10a9787052126fa4
Safraz Razik <safrazik@gmail.com>
2021-03-24 17:55:37 +0530

    Update network check

--- www/assets/js/app.js
+++ www/assets/js/app.js
@@ -1,3 +1,6 @@
+var isLocal =
+  !window.location.hostname || window.location.hostname == 'localhost' || window.location.hostname == '192.168.1.11';
+
 class App {
   constructor() {
     this.isDeviceReady = false;
@@ -6,18 +9,52 @@ class App {
     this.maxWaitingCount = 3;
     this.urlEditable = true;
     this.kioskMode = true;
+    this.checkInternetJsonp = {
+      jsonpCallback: 'checkInternet',
+      url: 'https://mdisplay.github.io/live/check-internet.js',
+      // url: 'http://192.168.1.11/mdisplay/live/check-internet.js',
+    };
     this.data = {
       showIntroMessages: true,
-      // url: 'https://mdisplay.github.io/live/',
-      url: 'http://192.168.1.11/mdisplay/live/',
+      url: 'https://mdisplay.github.io/live/',
+      // url: 'http://192.168.1.11/mdisplay/live/',
       exitCount: 0,
       version: '1.2.0',
       hello: 'World',
       initialized: false,
       editMode: false,
       waitingCount: 0,
+      network: {
+        status: 'Unknown',
+        checking: false,
+        checkingMessage: {
+          color: '',
+          text: '',
+        },
+      },
     };
     this.SETTINGS_STORAGE_KEY = 'mdisplay-launcher.settings';
+  }
+
+  checkNetworkStatus() {
+    if (!this.isDeviceReady || typeof Connection === 'undefined') {
+      return;
+    }
+    var networkState = navigator.connection.type;
+
+    var states = {};
+    states[Connection.UNKNOWN] = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI] = 'WiFi connection';
+    states[Connection.CELL_2G] = 'Cell 2G connection';
+    states[Connection.CELL_3G] = 'Cell 3G connection';
+    states[Connection.CELL_4G] = 'Cell 4G connection';
+    states[Connection.CELL] = 'Cell generic connection';
+    states[Connection.NONE] = 'No network connection';
+
+    this.data.network.status = states[networkState];
+
+    // alert('Connection type: ' + states[networkState]);
   }
 
   getExitCountDownMessage() {
@@ -137,6 +174,98 @@ class App {
     this.activateEditMode();
   }
 
+  setNetworkCheckingStatus(message, mode, status, timeout) {
+    const colors = {
+      init: '#ffff20',
+      error: '#ff1919',
+      success: '#49ff50',
+    };
+    setTimeout(
+      () => {
+        this.data.network.checkingMessage.text = message;
+        this.data.network.checkingMessage.color = colors[mode];
+      },
+      timeout ? 500 : 0
+    );
+    setTimeout(() => {
+      // this.fetchingInternetTime = status;
+      // this.data.network.checking = status;
+    }, timeout || 0);
+  }
+
+  checkInternetAvailability(okCallback) {
+    if (this.data.editMode || this.data.waitingCount > 0 || this.data.exitCount > 0) {
+      this.data.network.checking = false;
+      return;
+    }
+    this.checkNetworkStatus();
+    this.data.network.checking = true;
+    let url;
+    url = 'http://192.168.1.11/qurapp/qurapp/public/api/time';
+    url = 'http://192.168.1.11/non';
+    url = 'http://plaintext.qurapp.com/api/time'; // https won't work when time is invalid
+    url = 'https://qurapp.com/api/time';
+
+    this.setNetworkCheckingStatus('Checking Internet Connection...', 'init', true);
+    const retry = (okCallback) => {
+      setTimeout(() => {
+        this.checkInternetAvailability(okCallback);
+      }, 3000);
+    };
+    const useJsonp = true;
+    if (useJsonp) {
+      $.ajax({
+        type: 'GET',
+        dataType: 'jsonp',
+        url: this.checkInternetJsonp.url,
+        jsonpCallback: this.checkInternetJsonp.jsonpCallback,
+        contentType: 'application/json; charset=utf-8',
+        success: (response) => {
+          // console.log('Result received', response);
+          if (response && response.result == 'ok') {
+            this.setNetworkCheckingStatus('Internet Connection OK ', 'success', false, 1);
+            setTimeout(() => {
+              okCallback();
+              this.network.checking = false;
+            }, 1500);
+            return;
+          }
+          this.setNetworkCheckingStatus('INVALID response', 'error', false, 999);
+          retry(okCallback);
+        },
+        error: (err) => {
+          // console.log('err: ', err);
+          // // alert('err: ' + err);
+          this.setNetworkCheckingStatus('Internet Connection FAILED', 'error', false, 999);
+          retry(okCallback);
+        },
+      });
+      return;
+    }
+    ajax.get(url).then(
+      (response) => {
+        // alert('response : ' + response);
+        if (!(response && response.data)) {
+          console.log('Invalid response', response);
+          this.setNetworkCheckingStatus('INVALID response', 'error', false, 999);
+          retry(okCallback);
+          return;
+        }
+        this.setNetworkCheckingStatus('Internet Connection OK ', 'success', false, 1);
+        setTimeout(() => {
+          okCallback();
+          this.network.checking = false;
+        }, 1500);
+      },
+      (err) => {
+        console.log('err: ', err);
+        // alert('err: ' + err);
+        this.setNetworkCheckingStatus('Internet Connection FAILED - ' + err, 'error', false, 999);
+        retry(okCallback);
+      }
+    );
+  }
+
   doGoToUrl() {
     if (this.data.editMode) {
       return;
@@ -147,13 +276,15 @@ class App {
       }
       return;
     }
-    // finally!
-    cordova.InAppBrowser.open(
-      this.data.url,
-      '_self',
-      'location=no,hidenavigationbuttons=yes,hideurlbar=yes,hardwareback=yes,footer=no,fullscreen=' +
-        (this.kioskMode ? 'yes' : 'no')
-    );
+    this.checkInternetAvailability(() => {
+      // finally!
+      cordova.InAppBrowser.open(
+        this.data.url,
+        '_self',
+        'location=no,hidenavigationbuttons=yes,hideurlbar=yes,hardwareback=yes,footer=no,fullscreen=' +
+          (this.kioskMode ? 'yes' : 'no')
+      );
+    });
     //
   }
 
