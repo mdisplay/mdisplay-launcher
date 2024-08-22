3d4bf4f2954b6c54fb85271b2e32650981b3823f
Safraz Razik <safrazik@gmail.com>
2024-08-22 14:02:06 +0530

    Remove local sntp plugin

--- cordova-plugins-local-src/cordova-plugin-sntp/src/ios/CDVPluginSntp.h
+++
@@ -1,9 +0,0 @@
-#import <Cordova/CDV.h>
-
-@interface CDVPluginSntp : CDVPlugin
-
-- (void)setServer:(CDVInvokedUrlCommand*)command;
-- (void)getTime:(CDVInvokedUrlCommand*)command;
-- (void)getTimeOffset:(CDVInvokedUrlCommand*)command;
-
-@end
